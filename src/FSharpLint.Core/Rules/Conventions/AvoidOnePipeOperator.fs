f748957724038bc85d07b8bd002e807e9e8f601f
realmarv <zahratehraninasab@gmail.com>
2022-12-19 12:41:09 +0330

    FSharpLint.Core: implement new rule

--- src/FSharpLint.Core/Rules/Conventions/AvoidOnePipeOperator.fs
+++ src/FSharpLint.Core/Rules/Conventions/AvoidOnePipeOperator.fs
@@ -13,29 +13,26 @@ let runner (args: AstNodeRuleParams) =
     let error =
         match args.AstNode with
         | AstNode.Binding (SynBinding(synAccessOption, synBindingKind, mustInline, isMutable, synAttributeLists, preXmlDoc, synValData, headPat, synBindingReturnInfoOption, synExpr, range, debugPointAtBinding)) ->
-            match headPat with
-            | LongIdent (LongIdentWithDots(idents, dotRanges))
-            
-        match args.AstNode with
-        | AstNode.Expression (SynExpr.LetOrUse (_isRecursive, _isUse, bindings, body, _range)) ->
-            match List.tryHead bindings with
-            | Some(SynBinding(_synAccessOption, _synBindingKind, _mustInline, _isMutable, _synAttributeLists, _preXmlDoc, _synValData, headPat, _synBindingReturnInfoOption, _synExpr, _range, _debugPointAtBinding)) ->
-                match headPat with
-                | SynPat.Named(_synPat, ident, _isSelfIdentifier, _synAccessOption, _range) ->
-                    if ident.idText.StartsWith "_" then
-                        checkUsedIdent ident body
-                    else
-                        Array.empty
-                | _ ->
-                    Array.empty
+            match synExpr with
+            | SynExpr.App(exprAtomicFlag, isInfix, funcExpr, argExpr, range) ->
+                match funcExpr with
+                | SynExpr.App(exprAtomicFlag, isInfix, funcExpr, argExpr, range) ->
+                    match funcExpr with
+                    | SynExpr.Ident ident ->
+                        if ident.idText = "op_PipeRight" then
+                            {
+                                Range = range
+                                Message = String.Format(Resources.GetString ("RulesAvoidOnePipeOperator"))
+                                SuggestedFix = None
+                                TypeChecks = List.Empty
+                            } |> Array.singleton
+                        else
+                            Array.empty
+                    | _ -> Array.empty
+                | _ -> Array.empty
             | _ -> Array.empty
         | _ -> Array.empty
-        
-    error
-    printfn "%A" args.AstNode
-    printfn "--------------"
-    let error = Array.empty
-      
+    
     error
 
 let rule =
