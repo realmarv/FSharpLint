acfeb74f01183f03d318835e5aaf59103f76f81c
realmarv <zahratehraninasab@gmail.com>
2023-04-26 11:52:58 +0330

    FsharpLint.Core.Tests: add test for new rule
    Add test for AvoidUsingResultPropertyOfTaskVariable.

--- tests/FSharpLint.Core.Tests/Rules/Conventions/AvoidUsingResultPropertyOfTaskVariable.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/AvoidUsingResultPropertyOfTaskVariable.fs
@@ -14,3 +14,11 @@ let GetTaskOutput (task: Task<string>) =
     task.Result"""
 
         Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.``Using Async.AwaitTask instead of .Result property of a Task<'T> variable in a function``() =
+        this.Parse """
+let GetTaskOutput (task: Task<string>) =
+    Async.AwaitTask(task)"""
+
+        Assert.IsFalse this.ErrorsExist
