63b3d9e83e94b69192927be140cf25b1257227c9
realmarv <zahratehraninasab@gmail.com>
2023-04-29 16:10:06 +0330

    FSharpLint.Core.Tests: new failing test
    Add failing test for AvoidUsingResultPropertyOfTaskVariable.

--- tests/FSharpLint.Core.Tests/Rules/Conventions/AvoidUsingResultPropertyOfTaskVariable.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/AvoidUsingResultPropertyOfTaskVariable.fs
@@ -9,7 +9,10 @@ type TestConventionsAvoidUsingResultPropertyOfTaskVariable() =
 
     [<Test>]
     member this.``Avoid using .Result property of a Task<'T> variable in a function``() =
-        this.Parse """
+        this.Parse
+            """
+open System.Threading.Tasks
+
 let GetTaskOutput (task: Task<string>) =
     task.Result"""
 
@@ -17,8 +20,27 @@ let GetTaskOutput (task: Task<string>) =
 
     [<Test>]
     member this.``Using Async.AwaitTask instead of .Result property of a Task<'T> variable in a function``() =
-        this.Parse """
+        this.Parse
+            """
+open System.Threading.Tasks
+
 let GetTaskOutput (task: Task<string>) =
     Async.AwaitTask(task)"""
 
         Assert.IsFalse this.ErrorsExist
+
+    [<Test>]
+    member this.``Using .Result property of a Task<'T> variable when the Task type can't be inferred from the function signature``
+        ()
+        =
+        this.Parse
+            """
+open System.Threading.Tasks
+
+let GetTaskOutput task =
+    task.Result
+
+let task: Task<string> = client.GetStringAsync url
+let taskOutput = GetTaskOutput task"""
+
+        Assert.IsTrue this.ErrorsExist
