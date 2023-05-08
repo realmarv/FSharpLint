2de1244f3677846e5096dbe6d2de9dd0c9e187ad
realmarv <zahratehraninasab@gmail.com>
2023-05-08 15:55:53 +0330

    wip2

--- src/FSharpLint.Core/Rules/Conventions/AvoidUsingResultPropertyOfTaskVariable.fs
+++ src/FSharpLint.Core/Rules/Conventions/AvoidUsingResultPropertyOfTaskVariable.fs
@@ -2,6 +2,7 @@ module FSharpLint.Rules.AvoidUsingResultPropertyOfTaskVariable
 
 open System
 
+open System.Threading.Tasks
 open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 open FSharp.Compiler.Syntax
@@ -9,118 +10,78 @@ open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
 let runner (args: AstNodeRuleParams) =
+    match (args.AstNode, args.CheckInfo) with
+    | (AstNode.Identifier ([ identifierName; identifierProperty ], range), Some checkInfo) ->
+        let partialAssemblySignature = checkInfo.PartialAssemblySignature
+        let entity = partialAssemblySignature.Entities.[0]
 
-    // let errorsType1 =
-    //     match args.AstNode with
-    //     | Binding (SynBinding (_, _, _, _, _, _, _, headPat, _, expr, range, _)) ->
+        if identifierProperty <> "Result" then
+            Array.empty
+        else
+            entity.MembersFunctionsAndValues
+            |> Seq.map (fun item ->
+                match item.FullType.BaseType with
+                | Some baseType when
+                    baseType.ToString() = "type System.Threading.Tasks.Task"
+                    && item.DisplayName = identifierName
+                    ->
+                    { Range = range
+                      Message = Resources.GetString "RulesAvoidUsingResultPropertyOfTaskVariableError"
+                      SuggestedFix = None
+                      TypeChecks = List.Empty }
+                    |> Array.singleton
+                | _ -> Array.empty)
+            |> Array.concat
 
-    //         let (mayBeVariableName, mayBeVariableType) =
-    //             match headPat with
-    //             | SynPat.LongIdent
-    //                 (
-    //                     LongIdentWithDots ([ identifier ], _),
-    //                     _,
-    //                     _,
-    //                     SynArgPats.Pats ([ SynPat.Paren (SynPat.Typed (named, app, _), _) ]),
-    //                     _,
-    //                     _
-    //                 ) ->
-    //                 let variableName =
-    //                     match named with
-    //                     | SynPat.Named (_, ident: Ident, _, _, _) -> Some(ident.idText)
-    //                     | _ -> None
-
-    //                 let variableType =
-    //                     match app with
-    //                     | SynType.App (SynType.LongIdent (LongIdentWithDots ([ ident: Ident ], [])), _, _, _, _, _, _) ->
-    //                         Some(ident.idText)
-    //                     | _ -> None
-
-    //                 variableName, variableType
-    //             | _ -> None, None
-
-    //         match (mayBeVariableName, mayBeVariableType) with
-    //         | Some variableName, Some variableType when variableType = "Task" ->
-    //             let (mayBeSecondVariableName, mayBeSecondVariableProperty) =
-    //                 match expr with
-    //                 | SynExpr.LongIdent (_, LongIdentWithDots ([ task; result ], _), _, _) ->
-    //                     Some(task.idText), Some(result.idText)
-    //                 | _ -> None, None
-
-    //             match mayBeSecondVariableName, mayBeSecondVariableProperty with
-    //             | Some secondVariableName, Some secondVariableProperty when
-    //                 secondVariableName = variableName
-    //                 && secondVariableProperty = "Result"
-    //                 ->
-    //                 { Range = range
-    //                   Message = Resources.GetString "RulesAvoidUsingResultPropertyOfTaskVariableError"
-    //                   SuggestedFix = None
-    //                   TypeChecks = List.Empty }
-    //                 |> Array.singleton
-
-    //             | _ -> Array.empty
-    //         | _ -> Array.empty
-    //     | _ -> Array.empty
-    printfn "-----"
-    printfn "astnode:%A" args.AstNode
-    // printfn "syntaxarray:%A" args.SyntaxArray
-    let errorsType2 =
-        match (args.AstNode, args.CheckInfo) with
-        | (AstNode.Identifier ([ identifierName; identifierProperty ], range), Some checkInfo) ->
-            let partialAssemblySignature = checkInfo.PartialAssemblySignature
-            let entity = partialAssemblySignature.Entities.[0]
-
-            if identifierProperty <> "Result" then
-                Array.empty
-            else
-                entity.MembersFunctionsAndValues
-                |> Seq.map (fun item ->
-                    printfn "item:%A" item
-                    printfn "name:%A" item.DisplayName
-                    printfn "type:%A" item.FullType
-                    // printfn "type:%A" item.FullType.
-                    printfn "basetype:%A" item.FullType.BaseType
-                    match item.FullType.BaseType with
-                    | Some baseType when
-                        baseType.ToString() = "type System.Threading.Tasks.Task"
-                        && item.DisplayName = identifierName
-                        ->
-                        { Range = range
-                          Message = Resources.GetString "RulesAvoidUsingResultPropertyOfTaskVariableError"
-                          SuggestedFix = None
-                          TypeChecks = List.Empty }
-                        |> Array.singleton
-                    | _ -> Array.empty)
-                |> Array.concat
-        | AstNode.Binding(SynBinding(synAccessOption, synBindingKind, mustInline, isMutable, synAttributeLists, preXmlDoc, synValData, headPat, maybeSynBindingReturnInfoOption, synExpr, range, debugPointAtBinding)), Some checkInfo ->
-            let varName, varType =
-                match headPat with
-                | SynPat.LongIdent(longIdentWithDots, identOption, synValTyparDeclsOption, synArgPats, synAccessOption, range) ->
-                    match synArgPats with
-                    | SynArgPats.Pats synPats ->
-                        match synPats with
-                        | [SynPat.Paren(SynPat.Typed(SynPat.Named(synPat, ident, isSelfIdentifier, synAccessOption, _), SynType.App(SynType.LongIdent(LongIdentWithDots(idents, dotRanges)), rangeOption, typeArgs, commaRanges, greaterRange, isPostfix, _), _), _)] ->
-                            let varName = ident
-                            let varType = idents
-                            printfn "var: %A %A" varName varType
-                            Some(varName), Some(varType)
-                        | _ -> None, None
+    | AstNode.Binding (SynBinding (_, _, _, _, _, _, _, headPat, _, synExpr, range, _)), _ ->
+        let maybeVarName, maybeVarType =
+            match headPat with
+            | SynPat.LongIdent (_, _, _, synArgPats, _, _) ->
+                match synArgPats with
+                | SynArgPats.Pats synPats ->
+                    match synPats with
+                    | [ SynPat.Paren
+                            (
+                                SynPat.Typed
+                                    (
+                                        SynPat.Named (_, ident, _, _, _),
+                                        SynType.App
+                                            (
+                                                SynType.LongIdent (LongIdentWithDots (idents, _)), _, _, _, _, _, _
+                                            ),
+                                        _
+                                    ),
+                                _
+                            ) ] ->
+                        let varName = ident
+                        let varType = idents
+                        Some varName.idText, Some varType
                     | _ -> None, None
                 | _ -> None, None
-            
-            match maybeSynBindingReturnInfoOption with
-            | Some synBindingReturnInfoOption ->
-                match synBindingReturnInfoOption with
-                | SynBindingReturnInfoOption
-                    printfn "%A" synBindingReturnInfoOption
-                    
-                Array.empty
-            | _ -> Array.empty
-            
-        | _ -> Array.empty
+            | _ -> None, None
 
-    errorsType2
-    // Array.append errorsType1 errorsType2
+        let maybeSecondVarName, maybeSecondVarProperty =
+            match synExpr with
+            | SynExpr.LongIdent (_, LongIdentWithDots ([ secondVarName; secondVarProperty ], _), _, _) ->
+                Some secondVarName.idText, Some secondVarProperty.idText
+            | _ -> None, None
+
+        match maybeVarName, maybeVarType, maybeSecondVarName, maybeSecondVarProperty with
+        | Some varName, Some varType, Some secondVarName, Some secondVarProperty ->
+            printfn "info: %A %A %A %A" varName (varType.[3].idText = "Task") secondVarName secondVarProperty
+
+            if secondVarProperty = "Result"
+               && (varType.[3].idText = "Task")
+               && varName = secondVarName then
+                { Range = range
+                  Message = Resources.GetString "RulesAvoidUsingResultPropertyOfTaskVariableError"
+                  SuggestedFix = None
+                  TypeChecks = List.Empty }
+                |> Array.singleton
+            else
+                Array.empty
+        | _ -> Array.empty
+    | _ -> Array.empty
 
 let rule =
     { Name = "AvoidUsingResultPropertyOfTaskVariable"
